cd54812c27f1c1a6d52f0e01af182425bedc4972
mattmaximo <mattmaximo@hotmail.com>
2024-08-06 15:27:57 -0400

    fix

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="polymarket-py",  
-    version="0.1.0",
+    name="polymarket",  
+    version="0.1.1",
     description="A package to export historical odds for an outcome of a market on Polymarket",
     long_description=long_description,
     long_description_content_type="text/markdown",
